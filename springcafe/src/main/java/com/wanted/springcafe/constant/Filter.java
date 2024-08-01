d869b681e4b94c741a993776473c6b98849e0936
Alex <fnelclsrn123@gmail.com>
2024-08-02 00:03:16 +0900

    Delete springcafe directory

--- springcafe/src/main/java/com/wanted/springcafe/constant/Filter.java
+++
@@ -1,8 +0,0 @@
-package com.wanted.springcafe.constant;
-
-public class Filter {
-
-    public static String POST_FILTER = "deletedPostFilter";
-    public static String COMMENT_FILTER = "deletedCommentFilter";
-    public static String FILTER_PARAM = "isDeleted";
-}
--- springcafe/src/main/java/com/wanted/springcafe/domain/file/PostFileRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wanted.springcafe.domain.file;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostFileRepository extends JpaRepository<PostFileEntity, Long> {
-}
